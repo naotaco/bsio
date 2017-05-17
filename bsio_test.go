6c14b690d742803b29711e5183432762b41bd6c4
Nao Taco <naotaco@gmail.com>
2017-05-18 01:26:09 +0900

    add basic test

---
+++ bsio_test.go
@@ -0,0 +1,100 @@
+package bsio
+
+import (
+	//	"bsio"
+	"bytes"
+	"encoding/binary"
+	"github.com/pkg/errors"
+	"io"
+	"testing"
+)
+
+var testByteSeq = [][]byte{
+	[]byte{0x12, 0xab, 0x22, 0xff, 0x00, 0x34, 0x09, 0x01, 0x90, 0xaa},
+	[]byte{0},
+	[]byte{},
+}
+
+type bsData struct {
+	seq []byte   // raw byte sequence
+	b   [][]byte // expected data for 1~7 bit reading
+}
+
+var bsTestData = []bsData{
+	{
+		[]byte{0x12, 0x34},
+		[][]byte{
+			[]byte{}, // dummy
+			[]byte{0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0}, // 1-bit
+			[]byte{0}, // 2-bit
+			[]byte{0}, // ...
+			[]byte{0},
+			[]byte{0},
+			[]byte{0},
+			[]byte{0},
+		},
+	},
+}
+
+func TestStdBinaryCompatibility(t *testing.T) {
+
+	for _, b := range testByteSeq {
+		b_ := b
+		r1 := bytes.NewReader(b)
+		r2 := bytes.NewReader(b_)
+		bs := NewReader(r2, LittleEndian)
+		for {
+			var v1 byte
+			var v2 byte
+			err := binary.Read(r1, binary.LittleEndian, &v1)
+			err2 := bs.Read(&v2, 8)
+			// bsio.Read wraps io.EOF
+			if err == io.EOF && errors.Cause(err2) == io.EOF {
+				// succeed: both of Read functions should return EOF in same trial.
+				break
+			}
+			if v1 != v2 {
+				t.Logf("Unmatched read 8-bit data")
+				t.Fail()
+			} else {
+				// OK.
+				// fmt.Printf("v1 %x v2 %x\n", v1, v2)
+			}
+		}
+	}
+}
+
+func TestUint8(t *testing.T) {
+	for _, d_ := range bsTestData {
+		//		for _, bit := range []uint{1, 2, 3, 4, 5, 6, 7} {
+		for _, bit := range []uint{1} {
+
+			d := d_
+			r := bytes.NewReader(d.seq)
+			bs := NewReader(r, LittleEndian)
+
+			i := 0
+			for {
+				var v byte
+				err := bs.Read(&v, bit)
+				if errors.Cause(err) == io.EOF {
+					if i != len(d.b[bit]) {
+						t.Logf("Unmatched length: i: %d len: %d\n", i, len(d.b[bit]))
+						t.Fail()
+					}
+					t.Logf("i: %d len: %d\n", i, len(d.b[bit]))
+					break
+				}
+
+				if v != d.b[bit][i] {
+					t.Logf("Unmatched read %d-bit data. act: %x expected: %x", i, v, d.b[bit][i])
+					t.Fail()
+				} else {
+					// t.Logf("act: %x expected: %x\n", v, d.b1[i])
+				}
+
+				i++
+			}
+		}
+	}
+}
