da7a7abdae19f090895e10c58df80ace50e59292
Nao Taco <naotaco@gmail.com>
2017-05-19 21:17:59 +0900

    tmp

--- bsio_test.go
+++ bsio_test.go
@@ -22,16 +22,17 @@ type bsData struct {
 
 var bsTestData = []bsData{
 	{
+		//     0b00010010, 0b00110100
 		[]byte{0x12, 0x34},
 		[][]byte{
 			[]byte{}, // dummy
-			[]byte{0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0}, // 1-bit
-			[]byte{0}, // 2-bit
-			[]byte{0}, // ...
-			[]byte{0},
-			[]byte{0},
-			[]byte{0},
-			[]byte{0},
+			[]byte{0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0}, // 1-bit reading
+			[]byte{0, 1, 0, 2, 0, 3, 1, 0},                         // 2-bit
+			[]byte{0, 4, 4, 3, 2},                                  // ...
+			[]byte{1, 2, 3, 4},
+			[]byte{2, 8, 26},
+			[]byte{4, 35},
+			[]byte{9, 13},
 		},
 	},
 }
@@ -66,8 +67,7 @@ func TestStdBinaryCompatibility(t *testing.T) {
 
 func TestUint8(t *testing.T) {
 	for _, d_ := range bsTestData {
-		//		for _, bit := range []uint{1, 2, 3, 4, 5, 6, 7} {
-		for _, bit := range []uint{1} {
+		for _, bit := range []uint{1, 2, 3, 4, 5, 6, 7} {
 
 			d := d_
 			r := bytes.NewReader(d.seq)
@@ -87,7 +87,7 @@ func TestUint8(t *testing.T) {
 				}
 
 				if v != d.b[bit][i] {
-					t.Logf("Unmatched read %d-bit data. act: %x expected: %x", i, v, d.b[bit][i])
+					t.Logf("%d th data unmatched. act: %x expected: %x bit len: %x ", i, v, d.b[bit][i], bit)
 					t.Fail()
 				} else {
 					// t.Logf("act: %x expected: %x\n", v, d.b1[i])
